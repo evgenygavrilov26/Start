c71b748cc57d2e629c129e206c4df13cd0df78d1
evgenygavrilov26 <100408086+evgenygavrilov26@users.noreply.github.com>
2022-03-09 13:38:20 +0300

    Initial commit

---
+++ src/Start.java
@@ -0,0 +1,10 @@
+public class Start {
+    public static void main(String[] args) {
+        int k = 3;
+        int i = 1;
+        while (i<=10) {
+            System.out.println(k*i);
+            i++;
+        }
+    }
+}
