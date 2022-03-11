30349dee6d540d972ed8be0ce31f76fcd7ebd3ff
evgenygavrilov26 <100408086+evgenygavrilov26@users.noreply.github.com>
2022-03-11 14:07:17 +0300

    fist commit 2

---
+++ src/Next.java
@@ -0,0 +1,9 @@
+public class Next {
+    public static void main(String[] args) {
+        int k = 3;
+        for (int i = 1; i<=10;i++) {
+            System.out.println();
+            System.out.println();
+        }
+    }
+}
