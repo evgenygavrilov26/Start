aa4fdff78dff13502d20b3d20683d432c04f3da3
evgenygavrilov26 <100408086+evgenygavrilov26@users.noreply.github.com>
2022-03-11 14:12:14 +0300

    fist commit 4

--- src/Next.java
+++ src/Next.java
@@ -4,6 +4,7 @@ public class Next {
         for (int i = 1; i<=10;i++) {
             System.out.println();
             System.out.println();
+            System.out.println();
         }
     }
 }
--- src/Start.java
+++ src/Start.java
@@ -15,6 +15,7 @@ public class Start {
 
         }else {
             System.out.println(count2);
+            System.out.println(count2);
         }
     }
 }
